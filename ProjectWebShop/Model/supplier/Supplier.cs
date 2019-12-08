2b096a4d7c65ecfc52b38b1db044ab1c13e7f162
Hên Tạ <tavanhoanghen9999@gmail.com>
2019-12-08 11:31:41 +0700

    first commit

---
+++ ProjectWebShop/Model/supplier/Supplier.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectWebShop.Model.supplier
+{
+    public class Supplier
+    {
+        public int suplid { get; set; }
+        public string suplname { get; set; }
+        public string supladdress {get;set;}
+        public int suplphone { get; set; }
+        public Boolean status { get; set; }
+    }
+}
